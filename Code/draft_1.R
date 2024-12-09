efaf7bfd9459061c1f37c1d843867c102a834151
Gregor Lauth <162033115+GreckL@users.noreply.github.com>
2024-12-09 22:48:01 +0100

    Update

--- Code/draft_1.R
+++ Code/draft_1.R
@@ -1,16 +1,15 @@
 ###
+install.packages("baggr")
 library("bayesmeta")
 library("baggr")
 
 ### 1 - Simulating data ###
 n = 20
 tau_simul = runif(n,min = -1,max = 1)
-sigma_simul = runif(n,min = -.5,max = .5)
+sigma_simul = runif(n,min = .5,max = 5)
 
 
 
-
-library(baggr)
-df_pooled <- data.frame("tau" = c(28,8,-3,7,-1,1,18,12),
-                        "se"  = c(15,10,16,11,9,11,10,18))
-bg <- baggr(df_pooled, pooling = "partial")
+df_pooled <- data.frame("tau" = tau_simul,
+                        "se"  = sigma_simul)
+bg <- baggr(df_pooled, pooling = "full")
