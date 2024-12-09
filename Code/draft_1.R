76df1c415ac7b0b22fb8f9507d6f5a3a7d536b46
Gregor Lauth <162033115+GreckL@users.noreply.github.com>
2024-12-09 22:39:11 +0100

    Test

--- Code/draft_1.R
+++ Code/draft_1.R
@@ -6,3 +6,11 @@ library("baggr")
 n = 20
 tau_simul = runif(n,min = -1,max = 1)
 sigma_simul = runif(n,min = -.5,max = .5)
+
+
+
+
+library(baggr)
+df_pooled <- data.frame("tau" = c(28,8,-3,7,-1,1,18,12),
+                        "se"  = c(15,10,16,11,9,11,10,18))
+bg <- baggr(df_pooled, pooling = "partial")
