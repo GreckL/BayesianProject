afeb440ec5f49cde00252754b28ff2d4f278f0ed
Gregor Lauth <162033115+GreckL@users.noreply.github.com>
2024-12-10 16:05:20 +0100

    Update

--- Code/draft_1.R
+++ Code/draft_1.R
@@ -6,8 +6,9 @@ library("bayesmeta")
 library("baggr")
 library("bayesplot")
 library("ggplot2")        # For fancy plotting 
-library(tidyverse)        # Data manipulation 
-library(rstan)
+library("tidyverse")        # Data manipulation 
+library("rstan")
+library("compiler")
 rm(list=ls())
 set.seed(12345)
 
@@ -16,30 +17,26 @@ set.seed(12345)
 #tau_simul = runif(n,min = -1,max = 1)
 #sigma_simul = runif(n,min = .5,max = 5)
 
+
+################## Situation 1: DGM s.t. Fixed Effects ########################
+
+
 ### Start with frequentist weighted MLE
 # Parameters
-s=50         # Number of simulations
-nstud = 5   # Maximum number of studies
-
-theta_simul <- c()
-sigma_simul <- c()
-MLE_est <- c()
-MLE_mean <- c()
-MLE_MSE <- c()
-BAY_MSE <- c()
-bg <- c()
-BAY_tau <- c()
-bg_mse <- c()
+s=10         # Number of simulations
+nstud = 10   # Maximum number of studies
+theta_simul <- c()  #Simulate data set
+sigma_simul <- c()  #Simulate data set
+MLE_est <- c()    #Vector of simulated Frequentist weighted-pool Estimators
+MLE_mean <- c()   #Monte Carlo average 
+MLE_MSE <- c()    #Mean Squared Error of Frequentist approach
+BAY_MSE <- c()    #Mean Squared Error of Bayesian approach
+bg <- c()     #Bayesian Model
+BAY_tau <- c()  
+bg_mse <- c()   #Bayesian Mean Squared Error
 MSE_hat <- 0
 MSE_BAY <- c()
-theta_true <- 1
-
-# keep track
-pb <- txtProgressBar(min = 1,      # Minimum value of the progress bar
-                     max = s, # Maximum value of the progress bar
-                     style = 3,    # Progress bar style (also available style = 1 and style = 2)
-                     width = 50,   # Progress bar width. Defaults to getOption("width")
-                     char = "=")   # Character used to create the bar
+theta_true <- 1   #True Fixed Treatment Effect
 
 for (n in 2:nstud) {    # Number of studies (meta analysis so start from 2 up to nstud)
   for (i in 1:s) {      # Number of simulations
@@ -57,8 +54,7 @@ for (n in 2:nstud) {    # Number of studies (meta analysis so start from 2 up to
     # Bayesian
     df_pooled = data.frame("tau"= theta_simul,
                            "se" = sigma_simul)
-    ## Using partial
-    # Bayesian bagger
+    ## Using partial pooling from Bayesian bagger
     bg <- baggr(df_pooled, model="rubin", pooling = "partial")
     # Extract the Stan fit object
     fit_object <- bg$fit
@@ -70,16 +66,10 @@ for (n in 2:nstud) {    # Number of studies (meta analysis so start from 2 up to
     theta_k_rows <- fit_summary_df[grep("theta_k", rownames(fit_summary)), ]
     theta_SE <- (theta_k_rows$mean-1)^2
     BAY_MSE <- sum(unlist(theta_SE))/n
-    #if (s == 1) {
-    #  MSE_hat = BAY_MSE
-    #}
-    #else{
     MSE_hat = MSE_hat*((s-1)/s) + BAY_MSE*(1/s)
-    #}
     #*BAY_Nstud_MSE[i] <- sum(BAY_MSE)/n
     # Print the theta_k rows
     #print(theta_k_rows)
-    
   }
   MLE_mean[n] <- sum(MLE_est)/s
   MLE_MSE[n] <- (MLE_mean[n]-theta_true)^2
@@ -94,13 +84,9 @@ for (n in 2:nstud) {    # Number of studies (meta analysis so start from 2 up to
   #                        message = FALSE,
   #                        summary = TRUE)
   
-  #setTxtProgressBar(pb, t)
   #bg_mse$n = (tau_columns[[n]]-1)^2
   #BAY_MSE[n] = sum(unlist(bg_mse))/n
-  
 }
-close(pb)
-
 
 # Plot the MSEs to compare
 df <- data.frame(x=1:nstud, MLE_MSE, MSE_BAY)
@@ -120,13 +106,30 @@ p <- ggplot(df_long, aes(x = x, y = Value, color = Series, group = Series)) +
     legend.title = element_blank()
   ) +
   labs(
-    title = "MSE Comparison",
-    x = "Number of Studies",
+    title = "MSE Comparison in a Fixed Effects Setting",
+    x = "Number of Studies: n",
     y = "MSE"
   )
 
 # Display the plot
 x11()
 print(p)
+
+
+################### Situation 2: DGM s.t. Random Effects Model ###############
+
+
+
+
+
+
+
+### Things to modify:
+# Hyperpriors
+# Data Generating Model: Variance assumptions etc. 
+# 
+
+
+
 
 
